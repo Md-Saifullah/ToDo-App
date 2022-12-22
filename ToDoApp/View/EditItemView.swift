bb3dc1b37e1140d8907a3ae523c97904b2fef0bb
Mohammad Saifullah <mohammadsaifullah@Mohammads-Mac-mini.local>
2022-12-22 19:44:41 +0600

    Day-01 night 2nd

--- ToDoApp/View/EditItemView.swift
+++ ToDoApp/View/EditItemView.swift
@@ -8,49 +8,49 @@
 import SwiftUI
 
 struct EditItemView: View {
-   
     var item: ItemModel
-    @State var title:String=""
-    @State var description:String=""
-    init(item:ItemModel) {
-        UITextView.appearance().backgroundColor = .clear
-        self.item=item
-        // First, remove the UITextView's backgroundColor.
-    }
+    @State var title: String = ""
+    @State var description: String = ""
+    @State var isCompleted: Bool = false
+    @State var dueDate: Date = .now
+
     var body: some View {
-        ZStack{
-            VStack(alignment: .leading){
+        ZStack {
+            VStack(alignment: .leading) {
                 MultiSpacer(count: 1)
                 TextView(title: "Title:")
                 TextFieldView(background: Color("Background"), bindValue: $title)
                     .shadow(color: .gray, radius: 6)
-                
+
                 TextView(title: "Description:")
                 TextEditor(text: $description)
-//                    .font(.body)
-//                    .foregroundColor(.primary)
-//                    .background(Color.gray.opacity(0.8))
-//                    .autocorrectionDisabled(true)
-                                .frame(maxHeight: 200)
-                                .frame(maxWidth: .infinity)
-                                .cornerRadius(10)
-                                .shadow(color:.gray, radius: 6)
-                                .padding(.bottom)
+                    .frame(maxHeight: 200)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(10)
+                    .shadow(color: .gray, radius: 6)
+                    .padding(.bottom)
                 CustomButtonView(title: "Update", action: updateItem)
                 MultiSpacer(count: 3)
             }
             .navigationTitle("Add an Item 🖊️")
             .padding(30)
         }
+        .onAppear(perform: setScreen)
     }
-    func updateItem(){
-        
+    
+    func setScreen() {
+        title = item.title
+        description = item.description
+        isCompleted = item.isCompleted
+        dueDate = item.dueDate
     }
+    
+    func updateItem() {}
 }
 
 struct EditItemView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationStack{
+        NavigationStack {
             EditItemView(item: ItemModel(title: "first", description: "first description", dueDate: Date.now, isCompleted: false))
         }
     }
