52fd94076d96d0c5843095af3852fd19e07e65d9
Md. Saifullah <saif921921@gmail.com>
2023-01-25 18:42:31 +0600

    bugfix(keyboard): always show

--- ToDoApp/AppDelegate.swift
+++ ToDoApp/AppDelegate.swift
@@ -11,16 +11,16 @@ import UIKit
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         print("Your code here")
-
-        #if targetEnvironment(simulator)
+        
+#if targetEnvironment(simulator)
         // Disable hardware keyboards.
         let setHardwareLayout = NSSelectorFromString("setHardwareLayout:")
         UITextInputMode.activeInputModes
-            // Filter `UIKeyboardInputMode`s.
+        // Filter `UIKeyboardInputMode`s.
             .filter { $0.responds(to: setHardwareLayout) }
             .forEach { $0.perform(setHardwareLayout, with: nil) }
-        #endif
-
+#endif
+        
         return true
     }
 }
